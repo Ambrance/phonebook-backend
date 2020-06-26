497b2e0da5573148f74c177f658c6a2d2299ad20
Ambrance <ambrance@mail.ru>
2020-06-26 17:14:53 +0300

    feat: exercises 3.7-3.8 completed

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const morgan = require('morgan');
+
+morgan.token('data', (request) => JSON.stringify(request.body));
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data'),
+);
 let persons = [
   {
     name: 'Arto Hellas',
