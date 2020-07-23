139737bbc47a7f70c7e43cb3ef40d062dcded6df
Ambrance <ambrance@mail.ru>
2020-07-23 18:16:27 +0300

    uibuild

---
+++ build/precache-manifest.3a9456369368a27a1c882e3065fd0d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b7dc378475eb421ad3a5cbb64ee649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fd6c4dd5a899ccb8195",
+    "url": "/static/css/main.4e6dee19.chunk.css"
+  },
+  {
+    "revision": "8b4c8a609fa14e46aae0",
+    "url": "/static/js/2.8b094cab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b094cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd6c4dd5a899ccb8195",
+    "url": "/static/js/main.f26fda27.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
