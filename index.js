3653b5fbf722ee459b8901d10f18d6a88608818a
Ambrance <ambrance@mail.ru>
2020-08-21 19:06:52 +0300

    uibuild

--- index.js
+++ index.js
@@ -17,22 +17,24 @@ app.use(
 );
 
 app.get('/persons', (request, response) => {
-  Person.find({}).then((result) => {
-    response.json(result);
-    mongoose.connection.close();
-  });
+  Person.find({})
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/info', (request, response) => {
-  Person.find({}).then((result) => {
-    response.send(
-      `<div>
+  Person.find({})
+    .then((result) => {
+      response.send(
+        `<div>
     <p>Phonebook has info for ${result.length} persons.</p>
     <p>${new Date()}</p>
       </div>`,
-    );
-    mongoose.connection.close();
-  });
+      );
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/persons/:id', (request, response) => {
@@ -47,6 +49,20 @@ app.delete('/persons/:id', (request, response, next) => {
     .then((result) => {
       response.status(204).end();
     })
+    .catch((error) => next(error));
+});
+
+app.put('/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then((updatedPerson) => updatedPerson.json())
+
     .catch((error) => next(error));
 });
 
@@ -65,22 +81,36 @@ app.post('/persons', (request, response) => {
     });
   }
 
-  /*if (persons.some((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: `${body.name} already exist in the phonebook`,
-    });
-  }*/
-
   const person = new Person({
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((result) => {
-    response.json(result);
-    mongoose.connection.close();
-  });
+  person
+    .save()
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => next(error));
 });
+
+/*const unknownEndpoint = (request, result) => {
+  res.status(404).send({error: 'Unable to retrieve data.'});
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, result, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return res.status(400).send({error: 'Malformed id.'});
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({error: error.message});
+  }
+  next(error);
+};
+
+app.use(errorHandler);*/
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
